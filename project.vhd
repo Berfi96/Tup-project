9d95c7e099da11c4b52cb5bb3e71e5d075fd753e
Bartłomiej Fenik <34575137+Berfi96@users.noreply.github.com>
2019-01-26 10:01:53 +0100

    Update project.vhd

--- project.vhd
+++ project.vhd
@@ -5,10 +5,9 @@ use ieee.std_logic_unsigned.all;
 entity projekt is
 	port ( C : in std_logic;
 			clocK_50MHz: in std_logic;
-		RED2, RED1, RED0 , RED3, RED4, RED5, RED6, RED7: out std_logic;
-	GRN2, GRN1, GRN0, GRN3, GRN4, GRN5, GRN6, GRN7 : out std_logic;
-	BLU1, BLU0, BLU2, BLU3, BLU4, BLU5, BLU6, BLU7 : out std_logic;
-
+		RED2, RED1, RED0 : out std_logic;--, RED3, RED4, RED5, RED6, RED7: out std_logic;
+	GRN2, GRN1, GRN0 : out std_logic;--, GRN3, GRN4, GRN5, GRN6, GRN7 : out std_logic;
+	BLU1, BLU0 : out std_logic; --, BLU2 , BLU3, BLU4, BLU5, BLU6, BLU7 : out std_logic;
 	H_SYNC, V_SYNC : inout std_logic		);
 end projekt ;
 
@@ -26,9 +25,9 @@ SIGNAL counter_clock :std_logic_vector( 4 downto 0 ) := "00000";
 component vga is
 port( CLOCK_50MHz	: in std_logic;  -- zegar 50 MHz
 	RGB : in std_logic_vector(7 downto 0);
-	RED2, RED1, RED0 , RED3, RED4, RED5, RED6, RED7: out std_logic;
-	GRN2, GRN1, GRN0, GRN3, GRN4, GRN5, GRN6, GRN7 : out std_logic;
-	BLU1, BLU0, BLU2, BLU3, BLU4, BLU5, BLU6, BLU7 : out std_logic;
+	RED2, RED1, RED0 : out std_logic;-- , RED3, RED4, RED5, RED6, RED7: out std_logic;
+	GRN2, GRN1, GRN0 : out std_logic;--, GRN3, GRN4, GRN5, GRN6, GRN7 : out std_logic;
+	BLU1, BLU0 : out std_logic;--, BLU2, BLU3, BLU4, BLU5, BLU6, BLU7 : out std_logic;
 	H_SYNC, V_SYNC : inout std_logic  );
 end component;
 
@@ -58,32 +57,32 @@ end process;
 
 
 c1: vga port map (clock_50MHz=>clocK_50MHz,
-              				        RGB=>rgb,   --moze tutaj zmienic "=>" na "<=" ?
-						RED1 => red1,
-						RED3=>RED3,
-						RED4=>RED4,
-						RED5=>RED5,
-						RED6=>RED6,
-						RED7=>RED7,
-						red2 => red2,
-						grn0 => grn0,
-						grn1=>grn1,
-						grn2=>grn2,
-						GRN3=>GRN3,
-						GRN4=>GRN4,
-						GRN5=>GRN5,
-						GRN6=>GRN6,
-						GRN7=>GRN7,
-						blu0=>blu0,
-						blu1=>blu1,
-						BLU2=>BLU2,
-						BLU3=>BLU3,
-						BLU4=>BLU4,
-						BLU5=>BLU5,
-						BLU6=>BLU6,
-						BLU7=>BLU7,
-						h_sYNC=>h_sYNC,
-						v_sYNC=>v_sYNC);
+              				        RGB<=rgb,   --zmienione "=>" na "<=" ?
+						RED0 <= red0,
+						RED1<=RED1,
+						--RED4=>RED4,
+						--RED5=>RED5,
+						--RED6=>RED6,
+						--RED7=>RED7,
+						red2 <= red2,
+						grn0 <= grn0,
+						grn1<=grn1,
+						grn2<=grn2,
+						--GRN3=>GRN3,
+						--GRN4=>GRN4,
+						--GRN5=>GRN5,
+						--GRN6=>GRN6,
+						--GRN7=>GRN7,
+						blu0<=blu0,
+						blu1<=blu1,
+						--BLU2=>BLU2,
+						--BLU3=>BLU3,
+						--BLU4=>BLU4,
+						--BLU5=>BLU5,
+						--BLU6=>BLU6,
+						--BLU7=>BLU7,
+						h_sYNC<=h_sYNC,
+						v_sYNC<=v_sYNC);
 
 
 end Bech;
